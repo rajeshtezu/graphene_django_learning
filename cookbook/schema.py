97cd080732643c25337d28ad6692075d21554e7f
Rajesh Kumar <rajeshtezu90@gmail.com>
2018-05-27 12:18:41 +0530

    Initial commit. Added Category and Ingredient models. Created graphql schema. tested working

---
+++ cookbook/schema.py
@@ -0,0 +1,9 @@
+import graphene
+from ingredients import schema
+
+class Query(schema.Query, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+schema = graphene.Schema(query=Query)
