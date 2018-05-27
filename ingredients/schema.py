80924df4ccc07c913788ef00edc24ba3a2e32c30
Rajesh Kumar <rajeshtezu90@gmail.com>
2018-05-27 17:08:39 +0530

    Added category and ingredient resolvers

--- ingredients/schema.py
+++ ingredients/schema.py
@@ -11,11 +11,39 @@ class IngredientType(DjangoObjectType):
         model = Ingredient
 
 class Query(object):
+    category = graphene.Field(CategoryType, id=graphene.Int(), name=graphene.String())
     all_categories = graphene.List(CategoryType)
+
+    ingredient = graphene.Field(IngredientType, id=graphene.Int(), name=graphene.String())
     all_ingredients = graphene.List(IngredientType)
+
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Category.objects.get(pk=id)
+
+        if name is not None:
+            return Category.objects.get(name=name)
+            
+        return None
 
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
+
+    def resolve_ingredient(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Ingredient.objects.get(pk=id)
+
+        if name is not None:
+            return Ingredient.objects.get(name=name)
+
+        return None
+    
 
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.select_related('category').all()
